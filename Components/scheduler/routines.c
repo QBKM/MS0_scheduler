6cfc3eeed0363d06cd5e33807dbbecbe16085dc1
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-06-04 09:27:15 +0200

    add recovery system

--- Components/scheduler/routines.c
+++ Components/scheduler/routines.c
@@ -24,13 +24,35 @@
 
 #include "broadcast.h"
 
-//////////////////
-#include "gpio.h"
+#include "recsys.h"
+
+#include "config_file.h"
+
+/* ------------------------------------------------------------- --
+   defines
+-- ------------------------------------------------------------- */
+
+
+/* ------------------------------------------------------------- --
+   types
+-- ------------------------------------------------------------- */
+typedef struct 
+{
+    uint32_t time_ref;
+    uint32_t time_stamp;
+    uint8_t status;
+}REVERSE_t;
+
 
 /* ------------------------------------------------------------- --
    variables
 -- ------------------------------------------------------------- */
-phase_t phase;
+routines_t routines = {0};
+
+uint32_t time_sync = 0;
+
+REVERSE_t reverse_M1 ={0};
+REVERSE_t reverse_M2 ={0};
 
 /* ------------------------------------------------------------- --
    functions
@@ -42,7 +64,7 @@ phase_t phase;
  * ************************************************************* **/
 void phase_set(phase_t NewPhase)
 {
-    phase = NewPhase;
+    routines.phase = NewPhase;
 }
 
 /** ************************************************************* *
@@ -52,7 +74,7 @@ void phase_set(phase_t NewPhase)
  * ************************************************************* **/
 phase_t phase_get(void)
 {
-    return phase;
+    return routines.phase;
 }
 
 /** ************************************************************* *
@@ -61,7 +83,15 @@ phase_t phase_get(void)
  * ************************************************************* **/
 void routine_wait(void)
 {
-    
+    /* send a wait message every second */
+    if(HAL_GetTick() >= (time_sync + BROADCAST_WAIT_PHASE_DELAY))
+    {
+        time_sync = HAL_GetTick();
+        broadcast_uart_send(PHASE_WAIT);
+    }
+
+    routines.cycles.wait  += 1;
+    routines.cycles.total += 1;
 }
 
 /** ************************************************************* *
@@ -77,8 +107,18 @@ void routine_ascend(void)
     /* Temporal window [pooling mode]*/
     if(window_check_RTC_unlock() == true)
     {
-        // if apogee, => phase_set(PHASE_DEPLY)
+        // value to find
+        MPU6050_t IMU = MPU6050_Get_Struct();
+        if((IMU.KalmanAngleY >= 100) 
+        || (IMU.KalmanAngleX >= 100) 
+        || (IMU.Az <= 0))
+        {
+            phase_set(PHASE_DEPLOY);
+        }
     }
+
+    routines.cycles.ascend += 1;
+    routines.cycles.total  += 1;
 }
 
 /** ************************************************************* *
@@ -89,17 +129,120 @@ void routine_ascend(void)
  * ************************************************************* **/
 void routine_deploy(void)
 {
-    broadcast_uart(MSG_ID_phase_deploy);
-    phase_set(PHASE_DESCEND);
+    // first cycle tasks
+    if(routines.cycles.wait == 0)
+    {
+	    broadcast_uart_send(MSG_ID_phase_deploy);
+        broadcast_uart_send(MSG_ID_recsys_unlocking); 
+        broadcast_uart_send(MSG_ID_recsys_start_motor);
 
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, 0);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 1);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 1);
+        RECSYS_Unlock(RECSYS_M1 | RECSYS_M2);
+        RECSYS_Start(RECSYS_M1 | RECSYS_M2);
+    }
+    
+    RECSYS_t RECOVERY = RECSYS_Get_Struct();
 
+    /* test if ADC has trigged the IT flag */
+    if(RECOVERY.ADC.GLOBAL == true)
+    {
+        RECSYS_Stop(RECSYS_M1 | RECSYS_M2);
 
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, 1);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 1);
+        /* reading the motor sensor */
+        if(RECOVERY.PIN.CFDC_UNLOCK_M1 == HIGH)
+        {
+            RECSYS_set_Sys(RECSYS_M1, UNLOCKED);
+            RECSYS_Stop(RECSYS_M1);
+        }
+        else
+        {
+            RECSYS_set_Sys(RECSYS_M1, REVERSE);
+            
+            if(reverse_M1.status = false)
+            {
+                reverse_M1.status = true;
+                reverse_M1.time_ref = HAL_GetTick();
+                RECSYS_Lock(RECSYS_M1);
+            }
+
+            reverse_M1.time_stamp = HAL_GetTick();
+            if(reverse_M1.time_ref <= (reverse_M1.time_stamp + REVERSE_DELAY)) 
+            {
+                RECSYS_set_Sys(RECSYS_M1, UNLOCKING);
+                RECSYS_Unlock(RECSYS_M1);
+                reverse_M1.status = false;
+            }
+        }
+
+        if(RECOVERY.PIN.CFDC_UNLOCK_M2 == HIGH)
+        {
+            RECSYS_set_Sys(RECSYS_M2, UNLOCKED);
+            RECSYS_Stop(RECSYS_M2);
+        }
+        else
+        {
+            RECSYS_set_Sys(RECSYS_M2, REVERSE);
+            
+            if(reverse_M2.status = false)
+            {
+                reverse_M2.status = true;
+                reverse_M2.time_ref = HAL_GetTick();
+                RECSYS_Lock(RECSYS_M2);
+            }
+
+            reverse_M2.time_stamp = HAL_GetTick();
+            if(reverse_M2.time_ref <= (reverse_M2.time_stamp + REVERSE_DELAY))
+            {
+                RECSYS_set_Sys(RECSYS_M2, UNLOCKING);
+                RECSYS_Unlock(RECSYS_M2);
+                reverse_M2.status = false;
+            }
+        }
+
+        if((RECOVERY.SYS.M1 == UNLOCKED) && (RECOVERY.SYS.M2 == UNLOCKED))
+        {
+            RECSYS_set_Sys(RECSYS_GLOBAL, UNLOCKED);
+            broadcast_uart_send(MSG_ID_recsys_unlocked);
+            phase_set(PHASE_DESCEND);
+        }
+    }
+
+    if(RECOVERY.SYS.GLOBAL == UNLOCKED)
+    {
+        phase_set(PHASE_DESCEND);
+        broadcast_uart_send(MSG_ID_recsys_unlocked);
+    }
+    
+    //if(capteur_FDC() == true)
+    //{
+    //    // phase_set(PHASE_DESCEND);
+    //    // status => unlock
+    //    // uart => unlock FDC
+    //    broadcast_uart_send(MSG_ID_recsys_unlocked);
+    //}
+    //
+    //if(monitoring_current() == true)
+    //{
+    //    // stop the motors
+    //    // uart => stop motor
+    //    broadcast_uart_send(MSG_ID_recsys_stop_motor);
+    //
+    //    // check the FDC
+    //    if(capteur_FDC() == true)
+    //    {
+    //        // phase_set(PHASE_DESCEND);
+    //        // status => unlock
+    //        // uart => unlock MC
+    //        broadcast_uart_send(MSG_ID_recsys_unlocked);
+    //    }
+    //    if(capteur_FDC() == false)
+    //    {
+    //        // reverse motor for 500 ms
+    //        broadcast_uart_send(MSG_ID_recsys_reverse);
+    //    }
+    //}
+
+    routines.cycles.deploy += 1;
+    routines.cycles.total  += 1;
 }
 
 /** ************************************************************* *
@@ -111,6 +254,15 @@ void routine_descend(void)
 {
     /* read data MPU6050 */
 	MPU6050_Read_All_Kalman();
+
+    //MPU6050_t IMU = MPU6050_Get_Struct();
+    //if(IMU.Az >= ????????????)
+    //{
+    //    phase_set(PHASE_DEPLOY);
+    //}
+
+    routines.cycles.descend += 1;
+    routines.cycles.total   +=1;
 }
 
 /** ************************************************************* *
@@ -120,7 +272,8 @@ void routine_descend(void)
  * ************************************************************* **/
 void routine_landed(void)
 {
-	
+	routines.cycles.landed += 1;
+    routines.cycles.total  +=1;
 }
 
 /* ------------------------------------------------------------- --
