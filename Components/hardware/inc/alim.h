c1f92dbba57bd6cbab973c5f3fdeea2adf96ec0b
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-06-14 10:36:16 +0200

    add alim

---
+++ Components/hardware/inc/alim.h
@@ -0,0 +1,20 @@
+/** ************************************************************* *
+ * @file        alim.h
+ * @brief       
+ * 
+ * @date        2021-06-13
+ * @author      Quentin Bakrim (quentin.bakrim@hotmail.fr)
+ * 
+ * Mines Space
+ * 
+ * ************************************************************* **/
+
+#ifndef __ALIM_H__
+#define __ALIM_H__
+
+void ALIM_check_all(void);
+void ALIM_check_motor(void);
+void ALIM_check_seq(void);
+
+
+#endif
